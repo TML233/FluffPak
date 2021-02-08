ec6627e03f3d1a13ec0938d6243f66d1d9bf5d6e
TML <houjunhao33@163.com>
2021-02-08 20:43:35 +0800

    Change string length

--- Engine/Engine/System/String.cpp
+++ Engine/Engine/System/String.cpp
@@ -1,40 +1,46 @@
 #include "Engine/System/Definition.h"
 #include "Engine/System/String.h"
+#include "Engine/System/Exception.h"
 #include "Engine/Collection/Iterator.h"
+#include "Engine/System/IEnumerable.h"
 #include <string>
 #include <cstring>
 
 namespace Engine {
-	StringData::StringData(const char* data) {
-		int32 len = strlen(data);
-		length = len;
-
-		len += 1;
-		this->data = new char[len];
-		for (int32 i = 0; i < len; i += 1) {
-			this->data[i] = data[i];
-		}
+	StringData::StringData(const char* data, int32 count) {
+		length = count + 1;
+		this->data = new char[length];
+		// Copy data
+		std::memcpy(this->data, data, count);
+		// Add NULL
+		std::memset(this->data + count, 0, sizeof(char));
 	}
 	StringData::~StringData() {
 		delete[] data;
 	}
-	std::shared_ptr<StringData> StringData::empty = std::make_shared<StringData>("");
+	std::shared_ptr<StringData> StringData::empty = std::make_shared<StringData>("", sizeof(""));
 
 	String::String(const char* string) {
-		Prepare(string);
+		Prepare(string, std::strlen(string));
 	}
 	String::String(const std::string& string) {
-		Prepare(string.c_str());
+		Prepare(string.c_str(), string.length());
 	}
 	String& String::operator=(const char* string) {
-		Prepare(string);
+		Prepare(string, std::strlen(string));
 		return *this;
 	}
 
 	ReadonlyIterator<char> String::operator[](int32 index) const {
+		if (index >= data->length) {
+			throw ArgumentOutOfRangeException("index", "out of the range of the string.");
+		}
 		return ReadonlyIterator<char>(&(data->data[index]));
 	}
 
+	int32 String::GetCount() const {
+		return data->length - 1;
+	}
 	int32 String::GetLength() const {
 		return data->length;
 	}
@@ -44,24 +50,31 @@ namespace Engine {
 	}
 
 	int32 String::IndexOf(String pattern, int32 from, int32 length) const {
-		if (GetLength() < pattern.GetLength()) {
+		if (GetCount() < pattern.GetCount()) {
 			return -1;
 		}
-		return searcher.Search(GetRawArray(), GetLength(), pattern.GetRawArray(), pattern.GetLength());
+		return searcher.Search(GetRawArray(), GetCount(), pattern.GetRawArray(), pattern.GetCount());
 	}
 
+	/*String String::Substring(int32 startIndex, int32 count) const {
+		if (startIndex < 0) {
+			throw ArgumentOutOfRangeException("startIndex", "Out of range.");
+		}
+		if ((count < 0) || count > (GetCount() - startIndex)) {
+			throw ArgumentOutOfRangeException("count", "Out of range.");
+		}
+	}*/
 	String String::ToString() const {
 		return *this;
 	}
 
-	void String::Prepare(const char* string) {
-		int32 len = strlen(string);
+	void String::Prepare(const char* string,int32 count) {
 		// Use public empty string.
-		if (len <= 0) {
+		if (count <= 0) {
 			data = StringData::empty;
 			return;
 		}
-		data = std::make_shared<StringData>(string);
+		data = std::make_shared<StringData>(string,count);
 	}
 
 	String operator+(const String& left,const String& right) {
--- Engine/Engine/System/String.h
+++ Engine/Engine/System/String.h
@@ -14,24 +14,29 @@ namespace Engine {
 	class List;
 
 	struct StringData final {
-		explicit StringData(const char* data);
+		explicit StringData(const char* data,int32 count);
 		~StringData();
 
 		// Original data of the string.
 		char* data = nullptr;
-		// NULL not included.
+		// NULL included.
 		int length = 0;
 
 		static std::shared_ptr<StringData> empty;
 	};
 
+	// A string holding a NULL-termined char array.
 	class String final :public Object {
 	public:
 		String(const char* string = "");
 		String(const std::string& string);
 		String& operator=(const char* string);
 
-		// Get byte length. NULL not included.
+		// Get char count.
+		// NULL not included.
+		int32 GetCount() const;
+		// Get char array length.
+		// NULL included.
 		int32 GetLength() const;
 
 		// Get internal C-style char array.
@@ -42,27 +47,33 @@ namespace Engine {
 
 		// Find the position of the substring appearance in the current string.
 		// Return -1 if not found.
-		int32 IndexOf(String pattern,int32 from=0, int32 length=-1) const;
+		int32 IndexOf(String pattern, int32 from = 0, int32 length = -1) const;
 
 		bool StartsWith(String pattern) const;
 		bool EndsWith(String pattern) const;
+		String Substring(int startIndex, int count) const;
+		String Trim() const;
+		String TrimStart() const;
+		String TrimEnd() const;
 
-
+		// Format strings like "This is a {0}, except {1}";
 		template<typename ... Ts>
 		static String Format(const String& format, const Ts& ... args);
+		// Format strings like "This is a {0}, except {1}";
 		template<typename ... Ts>
 		static String Format(const char* format, const Ts& ... args);
 
 		String ToString() const override;
 
 	private:
-		void Prepare(const char* string);
+		// Prepares a string with a brand new data object.
+		void Prepare(const char* string, int32 count);
+		// Current data reference.
 		std::shared_ptr<StringData> data;
 
+		// Global sunday string searcher.
 		static StringSearcherSunday searcher;
 	};
-
-
 
 	template<typename ... Ts>
 	static String String::Format(const String& format, const Ts& ... args) {
