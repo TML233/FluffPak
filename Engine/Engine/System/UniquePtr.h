3b40f7e4c4217b3f3d15915a529ddda2927cd8ae
TML <houjunhao33@163.com>
2021-02-27 20:25:33 +0800

    Fix UniquePtr & Add moving

--- Engine/Engine/System/UniquePtr.h
+++ Engine/Engine/System/UniquePtr.h
@@ -8,120 +8,119 @@ namespace Engine {
 	template<typename T>
 	class DefaultDeleter {
 	public:
-		void operator()(T* ptr);
+		void operator()(T* ptr) {
+			MEMDEL(ptr);
+		}
 	};
-	template<typename T>
-	void DefaultDeleter<T>::operator()(T* ptr) {
-		MEMDEL(ptr);
-	}
 
 	// Default deleter for arrays which calls delete[]
 	template<typename T>
 	class DefaultDeleter<T[]> {
 	public:
-		void operator()(T* ptr);
+		void operator()(T* ptr) {
+			MEMDELARR(ptr);
+		}
 	};
-	template<typename T>
-	void DefaultDeleter<T[]>::operator()(T* ptr) {
-		MEMDELARR(ptr);
-	}
 
 	// UniquePtr
-	template<typename T,typename TDeleter=DefaultDeleter<T>>
+	template<typename T, typename TDeleter = DefaultDeleter<T>>
 	class UniquePtr final {
 	public:
-		explicit UniquePtr(T* ptr = nullptr);
-		~UniquePtr();
-		UniquePtr(const UniquePtr<T>& obj) = delete;
-		UniquePtr& operator=(const UniquePtr<T> obj) = delete;
-		T* GetRaw() const;
-		T* Release();
-		void Reset(T* ptr = nullptr);
-		T& operator*() const;
-		T* operator->() const;
-		operator bool();
+		explicit UniquePtr(T* ptr = nullptr) :ptr(ptr) {}
+		~UniquePtr() {
+			Reset();
+		}
+		
+		// No copying
+		UniquePtr(const UniquePtr& obj) = delete;
+		UniquePtr& operator=(const UniquePtr obj) = delete;
+		// Moving
+		UniquePtr(UniquePtr&& obj) {
+			ptr = obj.ptr;
+			obj.ptr = nullptr;
+		}
+		UniquePtr& operator=(UniquePtr&& obj) {
+			if (this == &obj) {
+				return *this;
+			}
+			ptr = obj.ptr;
+			obj.ptr = nullptr;
+			return *this;
+		}
+
+		T* GetRaw() const {
+			return ptr;
+		}
+		T* Release() {
+			T* ptr = this->ptr;
+			this->ptr = nullptr;
+			return ptr;
+		}
+		void Reset(T* ptr = nullptr) {
+			if (this->ptr != nullptr) {
+				deleter(this->ptr);
+			}
+			this->ptr = ptr;
+		}
+		T& operator*() const {
+			return *ptr;
+		}
+		T* operator->() const {
+			return ptr;
+		}
+		operator bool() {
+			return ptr != nullptr;
+		}
 	private:
 		T* ptr = nullptr;
 		TDeleter deleter;
 	};
-
-	template<typename T, typename TDeleter>
-	UniquePtr<T,TDeleter>::UniquePtr(T* ptr) :ptr(ptr) {}
-	template<typename T, typename TDeleter>
-	T* UniquePtr<T, TDeleter>::GetRaw() const {
-		return ptr;
-	}
-	template<typename T, typename TDeleter>
-	UniquePtr<T, TDeleter>::~UniquePtr() {
-		Reset();
-	}
-	template<typename T, typename TDeleter>
-	T* UniquePtr<T, TDeleter>::Release() {
-		T* ptr = this->ptr;
-		this->ptr = nullptr;
-		return ptr;
-	}
-	template<typename T, typename TDeleter>
-	void UniquePtr<T, TDeleter>::Reset(T* ptr) {
-		if (this->ptr != nullptr) {
-			deleter(this->ptr);
-		}
-		this->ptr = ptr;
-	}
-	template<typename T, typename TDeleter>
-	T& UniquePtr<T, TDeleter>::operator*() const {
-		return *ptr;
-	}
-	template<typename T, typename TDeleter>
-	T* UniquePtr<T, TDeleter>::operator->() const {
-		return ptr;
-	}
-	template<typename T, typename TDeleter>
-	UniquePtr<T, TDeleter>::operator bool() {
-		return ptr != nullptr;
-	}
 
 	// UniquePtr for arrays
 	template<typename T, typename TDeleter>
-	class UniquePtr<T[],TDeleter> {
+	class UniquePtr<T[], TDeleter> {
 	public:
-		explicit UniquePtr(T* ptr = nullptr);
-		~UniquePtr();
-		UniquePtr(const UniquePtr<T[]>& obj) = delete;
-		UniquePtr& operator=(const UniquePtr<T[]> obj) = delete;
-		T* GetRaw() const;
-		T* Release();
-		void Reset(T* ptr = nullptr);
-		operator bool();
+		explicit UniquePtr(T* ptr = nullptr) :ptr(ptr) {}
+		~UniquePtr() {
+			Reset();
+		}
+
+		// No copying
+		UniquePtr(const UniquePtr& obj) = delete;
+		UniquePtr& operator=(const UniquePtr obj) = delete;
+		// Moving
+		UniquePtr(UniquePtr&& obj) {
+			ptr = obj.ptr;
+			obj.ptr = nullptr;
+		}
+		UniquePtr& operator=(UniquePtr&& obj) {
+			if (this == &obj) {
+				return *this;
+			}
+			ptr = obj.ptr;
+			obj.ptr = nullptr;
+			return *this;
+		}
+
+		T* GetRaw() const {
+			return ptr;
+		}
+		T* Release() {
+			T* ptr = this->ptr;
+			this->ptr = nullptr;
+			return ptr;
+		}
+		void Reset(T* ptr = nullptr) {
+			if (this->ptr != nullptr) {
+				deleter(this->ptr);
+			}
+			this->ptr = ptr;
+		}
+		operator bool() {
+			return ptr != nullptr;
+		}
 	private:
 		T* ptr = nullptr;
 		TDeleter deleter;
 	};
-	template<typename T, typename TDeleter>
-	UniquePtr<T[], TDeleter>::UniquePtr(T* ptr) :ptr(ptr) {}
-	template<typename T, typename TDeleter>
-	T* UniquePtr<T[], TDeleter>::GetRaw() const {
-		return ptr;
-	}
-	template<typename T, typename TDeleter>
-	UniquePtr<T[], TDeleter>::~UniquePtr() {
-		deleter(ptr);
-	}
-	template<typename T, typename TDeleter>
-	T* UniquePtr<T[], TDeleter>::Release() {
-		T* ptr = this->ptr;
-		this->ptr = nullptr;
-		return ptr;
-	}
-	template<typename T, typename TDeleter>
-	void UniquePtr<T[], TDeleter>::Reset(T* ptr) {
-		if (this->ptr != nullptr) {
-			deleter(this->ptr);
-		}
-		this->ptr = ptr;
-	}
-	template<typename T, typename TDeleter>
-	UniquePtr<T[], TDeleter>::operator bool() {
-		return ptr != nullptr;
-	}
 }
