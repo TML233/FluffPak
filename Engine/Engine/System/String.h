a1750b3335e6c9cb5f46444131f2ced3e893d505
TML <houjunhao33@163.com>
2021-04-24 14:30:49 +0800

    Doxygen comment style for String

--- Engine/Engine/System/String.h
+++ Engine/Engine/System/String.h
@@ -10,89 +10,119 @@
 #include "Engine/Collection/List.h"
 #include <string_view>
 
-// Defines a String literal.
-#define STRING_LITERAL(text)																							\
-([](){																													\
-	static const char content[]=text;																					\
-	static const ::Engine::String::ContentData data(content, sizeof(content));												\
+/// @brief Make a String literal.
+/// This prevents allocating heap memory for constant strings.
+#define STRING_LITERAL(text)																											\
+([](){																																	\
+	static const char content[]=text;																									\
+	static const ::Engine::String::ContentData data(content, sizeof(content));															\
 	return ::Engine::String(::Engine::ReferencePtr<::Engine::String::ContentData>(const_cast<::Engine::String::ContentData*>(&data)));	\
 })()
 
 namespace Engine {
-	// A string holding a NULL-termined char array.
+	/// @brief A string holding a NULL-termined char array.
+	/// The actual content is reference counted, so it's cheap to copy around.
 	class String final {
 	public:
-		// Container of actual data of Strings. Shared between Strings.
-		// Does not copy any data.
+		/// @brief Container of actual content data of Strings. Shared between Strings. 
 		struct ContentData final {
-			// Accept data as a static block. Will not free the data.
-			// Only for internal use. Used by STRING_LITERAL.
-			// Must NOT be allocated on heap, or the memory leak would occur.
+			/// @brief Accept data as a static block. Will not free the data.
+			///	Only for internal use. Used by STRING_LITERAL.
+			/// @param data A static string data block. Can't be allocated on heap.
+			/// @param length The length of the given string data block. NULL included.
 			ContentData(const char* data, int32 length);
 
-			// Accept data as a memory block on heap. Will free the data.
+			/// @brief Accept data as a memory block on heap. Will free the data.
+			/// @param data A UniquePtr holding string data block on heap. Use Memory::Move to "move" it in.
+			/// @param length The length of the given string data block. NULL included.
 			ContentData(UniquePtr<char[]>&& data, int32 length);
 
 			~ContentData();
 
-			// Original data of the string.
 			const char* data = nullptr;
-			// NULL included.
+
+			/// @brief NULL included.
 			int length = 0;
 
 			uint32 Reference() const;
 			uint32 Dereference() const;
 			uint32 GetReferenceCount() const;
 
+			/// @brief Get the global empty content data.
 			static ReferencePtr<ContentData> GetEmpty();
 		private:
 			bool staticData;
 			mutable ReferenceCount referenceCount;
 		};
 
+		/// @brief Get the global empty String.
 		static String GetEmpty();
 
 #pragma region ctor/assignment
-		// Creates a string from a raw string.
-		// Will copy the original string.
+		/// @brief Creates a String from a raw C-Style string.
+		/// Will make a copy of the content.
+		/// @param string The C-Style string.
+		/// @param count The char count. NULL NOT included. -1 for auto detection.
 		String(const char* string = "", int32 count = -1);
 		
 		String& operator=(const char* string);
 
-		// Creates a string from a std::string.
-		// Will copy the original string.
 		String(const std::string& string);
 
-		// Creates a string using a ContentData.
-		// Will not copy.
+		/// @brief Creates a String using a pre-allocated ContentData.
+		/// Will not do any copy.
+		/// @param dataPtr The pre-allocated ContentData.
+		/// @param start The start index of referencing content.
+		/// @param count The char count for referencing content. NULL NOT included. -1 for auto detection.
 		String(ReferencePtr<ContentData> dataPtr, int32 start = 0, int32 count = -1);
 #pragma endregion
 
 #pragma region Tool functions
-		// Get char count.
-		// NULL not included.
+		/// @brief Get char count. NULL NOT included.
 		int32 GetCount() const;
 
-		// Get internal C-style char array.
-		// Do not store the pointer.
+		/// @brief Get the internal C-Style string array pointer. Do not store the pointer.
 		const char* GetRawArray() const;
 
+		/// @brief Check if the string is a individual one.
+		/// Individual string means that the content of this string is exactally the same as the underlying raw string array.
 		bool IsIndividual() const;
+		
+		/// @return A individual string whose content is the same as current string.
+		/// If current string is already individual, return self.
 		String ToIndividual() const;
 
+		/// @brief Get the char at the given index.
 		ReadonlyIterator<char> operator[](int32 index) const;
 
-		// Find the position of the substring appearance in the current string.
-		// If count == -1, search the whole string.
-		// Return -1 if not found.
+		/// @brief FindFind the position of the substring appearance in the string.
+		/// @param pattern The substring to search.
+		/// @param startFrom The index to start searching from.
+		/// @param count The count of chars from the start index to search.
+		/// -1 to search to the end of the string.
+		/// @return The position of the substring occuring in the string.
+		/// -1 if not found.
 		int32 IndexOf(const String& pattern,int32 startFrom=0,int32 count=-1) const;
 
-		// Check if the current string contains another string.
+		/// @brief Check if the string contains another string.
+		/// @param pattern The substring to search.
 		bool Contains(const String& pattern) const;
 
+		/// @brief Check if the string starts with another string.
+		/// @param pattern The substring to search.
 		bool StartsWith(const String& pattern) const;
+
+		/// @brief Check if the string ends with another string.
+		/// @param pattern The substring to search.
 		bool EndsWith(const String& pattern) const;
+
+		/// @param startIndex The index to start cutting from.
+		/// @param count The count of chars from the start index to cut.
 		String Substring(int32 startIndex, int32 count) const;
+
+		/// @brief Replace a substring to another.
+		/// @param from The substring to be replaced.
+		/// @param to The string to be replaced to.
 		String Replace(const String& from, const String& to) const;
 
 		String ToString() const;
@@ -106,12 +136,11 @@ namespace Engine {
 #pragma endregion
 
 #pragma region Format
-		// Format strings like "This is a {0}, except {1}";
 		template<typename ... Ts>
 		static String Format(const String& format, const Ts& ... args) {
 			return fmt::format(format.ToIndividual().GetRawArray(), args...);
 		}
-		// Format strings like "This is a {0}, except {1}";
+
 		template<typename ... Ts>
 		static String Format(const char* format, const Ts& ... args) {
 			return fmt::format(format, args...);
@@ -123,17 +152,16 @@ namespace Engine {
 	private:
 		bool IsEqual(const String& obj) const;
 
-		// Prepares a string, the string data will be copied.
-		// Count does not accept -1.
+		/// @brief Prepares a string, the string data will be copied.
+		/// Count does not accept -1.
 		void PrepareData(const char* string, sizeint count);
 
-		// Current data reference.
 		ReferencePtr<ContentData> data;
 
 		int32 refStart = 0;
 		int32 refCount = 0;
 
-		// Global sunday string searcher.
+		/// @brief Global sunday string searcher.
 		static StringSearcherSunday searcher;
 
 		static List<sizeint> replacerIndexes;
