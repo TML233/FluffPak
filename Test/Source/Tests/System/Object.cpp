41450f7bc72269f27bb6ec8354fb73abc4ea448f
TML <houjunhao33@163.com>
2021-05-11 10:47:54 +0800

    Add more tests for Signal

--- Test/Source/Tests/System/Object.cpp
+++ Test/Source/Tests/System/Object.cpp
@@ -21,13 +21,38 @@ public:
 };
 
 TEST_CASE("Object") {
+	String signame = STRL("TestSignal");
+	String recvname = STRL("OnTestSignal");
 	UniquePtr<SignalTest> sig = UniquePtr<SignalTest>::Create();
-	UniquePtr<SignalHandler> hd = UniquePtr<SignalHandler>::Create();
-	sig->ConnectSignal(STRL("TestSignal"), Invokable(hd.GetRaw(), STRL("OnTestSignal")));
+
+	UniquePtr<SignalHandler> hd1 = UniquePtr<SignalHandler>::Create();
+	CHECK(sig->ConnectSignal(signame, Invokable(hd1.GetRaw(), recvname)) == ReflectionSignal::ConnectResult::OK);
+	UniquePtr<SignalHandler> hd2 = UniquePtr<SignalHandler>::Create();
+	CHECK(sig->ConnectSignal(signame, Invokable(hd2.GetRaw(), recvname)) == ReflectionSignal::ConnectResult::OK);
+	UniquePtr<SignalHandler> hd3 = UniquePtr<SignalHandler>::Create();
+	CHECK(sig->ConnectSignal(signame, Invokable(hd3.GetRaw(), recvname)) == ReflectionSignal::ConnectResult::OK);
+
+	CHECK(sig->IsSignalConnected(signame, Invokable(hd1.GetRaw(), recvname)));
+	CHECK(sig->IsSignalConnected(signame, Invokable(hd2.GetRaw(), recvname)));
+	CHECK(sig->IsSignalConnected(signame, Invokable(hd3.GetRaw(), recvname)));
+
+	CHECK(!sig->IsSignalConnected(signame, Invokable(hd1.GetRaw(), STRL("SB"))));
+	CHECK(!sig->IsSignalConnected(STRL("SB"), Invokable(hd2.GetRaw(), recvname)));
 
 	Variant v = 3;
 	const Variant* args[] = { &v };
-	sig->EmitSignal(STRL("TestSignal"), (const Variant**)args, 1);
+	CHECK(sig->EmitSignal(signame, (const Variant**)args, 1));
 
-	CHECK(hd->value == 3);
+	CHECK(hd1->value == 3);
+	CHECK(hd2->value == 3);
+	CHECK(hd3->value == 3);
+
+	CHECK(sig->DisconnectSignal(signame, Invokable(hd2.GetRaw(), recvname)));
+
+	v = -1;
+	CHECK(sig->EmitSignal(signame, (const Variant**)args, 1));
+
+	CHECK(hd1->value == 2);
+	CHECK(hd2->value == 3);
+	CHECK(hd3->value == 2);
 }
