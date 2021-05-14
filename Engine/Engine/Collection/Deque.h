79948fec07c3aecf0cc91ea57f48009289a34e6f
TML <houjunhao33@163.com>
2021-05-14 14:20:50 +0800

    Add Deque

--- Engine/Engine/Collection/Deque.h
+++ Engine/Engine/Collection/Deque.h
@@ -7,6 +7,10 @@ namespace Engine {
 	template<typename T>
 	class Deque final {
 	public:
+		~Deque() {
+			Clear();
+		}
+
 		void PushFront(const T& value) {
 			int32 chunkIndex = PrepareChunk(GetChunkIndex(frontIndex));
 			SharedPtr<ElementChunk> chunk = chunks.Get(chunkIndex);
@@ -25,12 +29,44 @@ namespace Engine {
 
 			backIndex += 1;
 		}
-		//bool TryPopFront(T& result) {
-		//
-		//}
-		//bool TryPopBack(T& result) {
-		//
-		//}
+		bool TryPopFront(T& result) {
+			if (GetCount() <= 0) {
+				return false;
+			}
+			frontIndex += 1;
+			
+			int32 chunkIndex = GetChunkIndex(frontIndex);
+			SharedPtr<ElementChunk> chunk = chunks.Get(chunkIndex);
+			result = chunk->elements[GetElementIndexInChunk(frontIndex)];
+
+			Memory::Destruct(chunk->elements + GetElementIndexInChunk(frontIndex));
+			return true;
+		}
+		bool TryPopBack(T& result) {
+			if (GetCount() <= 0) {
+				return false;
+			}
+			backIndex -= 1;
+
+			int32 chunkIndex = GetChunkIndex(backIndex);
+			SharedPtr<ElementChunk> chunk = chunks.Get(chunkIndex);
+			result = chunk->elements[GetElementIndexInChunk(backIndex)];
+
+			Memory::Destruct(chunk->elements + GetElementIndexInChunk(backIndex));
+			return true;
+		}
+		T PopFront() {
+			T result;
+			bool succeeded = TryPopFront(result);
+			ERR_ASSERT(succeeded, u8"Cannot pop from front!", return T());
+			return result;
+		}
+		T PopBack() {
+			T result;
+			bool succeeded = TryPopBack(result);
+			ERR_ASSERT(succeeded, u8"Cannot pop from back!", return T());
+			return result;
+		}
 		int32 GetCount() const {
 			return backIndex - frontIndex - 1;
 		}
@@ -45,6 +81,9 @@ namespace Engine {
 				}
 				Memory::Destruct(lastChunk->elements + GetElementIndexInChunk(i));
 			}
+
+			frontIndex = -1;
+			backIndex = 0;
 		}
 
 		static const inline int32 ChunkSize = 8;
--- Test/Source/Tests/Collection/Deque.cpp
+++ Test/Source/Tests/Collection/Deque.cpp
@@ -1,15 +1,80 @@
 #include "doctest.h"
 #include "Engine/Collection/Deque.h"
+#include "../System/MemoryObject.h"
 
 using namespace Engine;
 
 TEST_SUITE("Collections") {
 	TEST_CASE("Deque") {
-		Deque<int32> deque;
+		Deque<MemoryObject> deque;
 		deque.PushBack(1);
 		deque.PushBack(2);
 		deque.PushBack(3);
 		deque.PushFront(0);
 		deque.PushFront(-1);
+		CHECK(deque.GetCount() == 5);
+
+
+		// -1 0 1 2 3
+		bool s = deque.PopBack() == MemoryObject(3);
+		CHECK(s);
+		CHECK(deque.GetCount() == 4);
+		
+		// -1 0 1 2
+		s = deque.PopFront() == MemoryObject(-1);
+		CHECK(s);
+		CHECK(deque.GetCount() == 3);
+
+		// 0 1 2
+		s = deque.PopBack() == MemoryObject(2);
+		CHECK(s);
+		CHECK(deque.GetCount() == 2);
+
+		// 0 1
+		deque.PushBack(9);
+		CHECK(deque.GetCount() == 3);
+
+		// 0 1 9
+		s = deque.PopFront() == MemoryObject(0);
+		CHECK(s);
+		CHECK(deque.GetCount() == 2);
+
+		// 1 9
+		s = deque.PopBack() == MemoryObject(9);
+		CHECK(s);
+		CHECK(deque.GetCount() == 1);
+
+		// 1
+		s = deque.PopFront() == MemoryObject(1);
+		CHECK(s);
+		CHECK(deque.GetCount() == 0);
+
+		MemoryObject obj;
+		CHECK(!deque.TryPopBack(obj));
+		CHECK(!deque.TryPopFront(obj));
+
+		deque.PushFront(8);
+		CHECK(deque.GetCount() == 1);
+
+		s = deque.PopBack() == MemoryObject(8);
+		CHECK(s);
+		CHECK(deque.GetCount() == 0);
+
+		deque.PushBack(114);
+		CHECK(deque.GetCount() == 1);
+		
+		s = deque.PopFront() == MemoryObject(114);
+		CHECK(s);
+		CHECK(deque.GetCount() == 0);
+
+
+		deque.PushBack(0);
+		deque.PushFront(1);
+		deque.PushBack(2);
+		deque.PushFront(3);
+		deque.PushBack(4);
+		deque.PushFront(5);
+		deque.PushBack(6);
+		CHECK(deque.GetCount() == 7);
 	}
 }
