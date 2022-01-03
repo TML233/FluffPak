b4e905e05d9c9cf89d7bda2eaacc45ce3b398820
TML <houjunhao33@163.com>
2022-01-03 19:29:56 +0800

    Remove Legacy Linux Support

--- Engine/Engine/Platform/Window.h
+++ Engine/Engine/Platform/Window.h
@@ -4,5 +4,5 @@
 #if CURRENT_PLATFORM_WINDOWS
 #	include "Engine/Platform/Windows/Window.h"
 #elif CURRENT_PLATFORM_LINUX
-#   include "Engine/Platform/Linux/Window.h"
+//#   include "Engine/Platform/Linux/Window.h"
 #endif
