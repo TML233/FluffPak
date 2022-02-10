7738320ee3ec54583eb0f2de3920aac4933ab343
TML <houjunhao33@163.com>
2022-02-10 12:00:50 +0800

    Use std::source_location

--- Engine/Engine/System/Debug.h
+++ Engine/Engine/System/Debug.h
@@ -1,29 +1,24 @@
 #pragma once
 #include "Engine/System/Definition.h"
+#include <source_location>
 
 #define DEBUG_ENABLED true
 
-#if(DEBUG_ENABLED)
-#define DEBUG_TEXT(text) text
-#else
-#define DEBUG_TEXT(text) ""
-#endif
-
 #define INFO_MSG(msg) ::Engine::DebugPrintInfo(msg)
 
-#define WARN_MSG(msg) ::Engine::DebugPrintWarn(__func__,__FILE__,__LINE__,DEBUG_TEXT(msg))
+#define WARN_MSG(msg) ::Engine::DebugPrintWarn(std::source_location::current().function_name(),std::source_location::current().file_name(),std::source_location::current().line(),msg)
 
-#define ERR_MSG(msg) ::Engine::DebugPrintError(__func__,__FILE__,__LINE__,DEBUG_TEXT(msg))
-#define ERR_ASSERT(expr,msg,action) if (!(expr)) { ::Engine::DebugPrintErrorAssert(__func__,__FILE__,__LINE__,#expr,msg,#action);action;}
+#define ERR_MSG(msg) ::Engine::DebugPrintError(std::source_location::current().function_name(),std::source_location::current().file_name(),std::source_location::current().line(),msg)
+#define ERR_ASSERT(expr,msg,action) if (!(expr)) { ::Engine::DebugPrintErrorAssert(std::source_location::current().function_name(),std::source_location::current().file_name(),std::source_location::current().line(),#expr,msg,#action);action;}
 
 #ifdef _MSC_VER
 #define FATAL_CRASH_IMMEDIATELY() __debugbreak()
 #else
 #define FATAL_CRASH_IMMEDIATELY() __builtin_trap()
 #endif
-#define FATAL_MSG(msg) ::Engine::DebugPrintFatal(__func__,__FILE__,__LINE__,DEBUG_TEXT(msg))
+#define FATAL_MSG(msg) ::Engine::DebugPrintFatal(std::source_location::current().function_name(),std::source_location::current().file_name(),std::source_location::current().line(),msg)
 #define FATAL_CRASH(msg) FATAL_MSG(msg);FATAL_CRASH_IMMEDIATELY()
-#define FATAL_ASSERT(expr,msg) if (!(expr)) { ::Engine::DebugPrintFatalAssert(__func__,__FILE__,__LINE__,#expr,msg);FATAL_CRASH_IMMEDIATELY();}
+#define FATAL_ASSERT(expr,msg) if (!(expr)) { ::Engine::DebugPrintFatalAssert(std::source_location::current().function_name(),std::source_location::current().file_name(),std::source_location::current().line(),#expr,msg);FATAL_CRASH_IMMEDIATELY();}
 
 namespace Engine {
 	void DebugPrintInfo(const u8char* message);
